c304a14b5beda025e8780a53929d531963de258a
Andre <andre.babirian2004@gmail.com>
2026-04-21 23:26:29 +0200

    2de commit begonnen met mijn movie dashboard website

---
+++ src/pages/main.tsx
@@ -0,0 +1,12 @@
+import '../styling/main.css'
+
+function MainPage() {
+  return (
+    <div className="main-page">
+      <h1>Welkom</h1>
+      <p>Dit is de hoofdpagina.</p>
+    </div>
+  )
+}
+
+export default MainPage
