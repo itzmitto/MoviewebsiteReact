e385c7494aa89cb01c811d123f7f7091076b2883
Andre <andre.babirian2004@gmail.com>
2026-04-23 19:42:45 +0200

    6de commit gewerkt aan de website

---
+++ src/pages/main.tsx
@@ -0,0 +1,23 @@
+import '../styling/main.css'
+import Sidebar from "./sidebar";
+
+
+
+function Main() {
+  return (
+    <div className="layout">
+        <Sidebar /> 
+      <div className="main-page">
+        <h1>Dit is het begin</h1>
+        <p>Welkom op je mijn school dashboard</p>
+      </div>
+
+
+
+
+
+    </div>
+  )
+}
+
+export default Main 
---
+++ src/pages/sidebar.tsx
@@ -0,0 +1,17 @@
+function Sidebar() {
+  return (
+    <div className="sidebar">
+      <h2 className="logo">🎬 MovieApp</h2>
+      <nav>
+        <ul>
+          <li>Home</li>
+          <li>Populaire films</li>
+          <li>Genres</li>
+          <li>Favorieten</li>
+        </ul>
+      </nav>
+    </div>
+  );
+}
+
+export default Sidebar;
