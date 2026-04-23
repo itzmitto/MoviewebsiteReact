04acf894beb5813651142ef02e7b6c6fc3b63e76
Andre <andre.babirian2004@gmail.com>
2026-04-23 19:50:50 +0200

    7de commit gewerkt aan de website

--- src/pages/sidebar.tsx
+++ src/pages/sidebar.tsx
@@ -1,17 +1,19 @@
 function Sidebar() {
-  return (
-    <div className="sidebar">
-      <h2 className="logo">🎬 MovieApp</h2>
-      <nav>
-        <ul>
-          <li>Home</li>
-          <li>Populaire films</li>
-          <li>Genres</li>
-          <li>Favorieten</li>
-        </ul>
-      </nav>
-    </div>
-  );
+    return (
+        <div className="sidebar">
+            <h2 className="logo">MoonMovieTv</h2>
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>Populaire films</li>
+                    <li>Genres</li>
+                    <li>Trending</li>
+                    <li>Playlist</li>
+                    <li>Favorieten</li>
+                </ul>
+            </nav>
+        </div>
+    );
 }
 
 export default Sidebar;
