2d5aba585a8c91b9a0d557d74ac92c65338d8c3c
Andre <andre.babirian2004@gmail.com>
2026-04-28 21:36:35 +0200

    10

--- src/pages/sidebar.tsx
+++ src/pages/sidebar.tsx
@@ -6,8 +6,8 @@ function Sidebar() {
             <hr className="sidebar-divider" />
 
             <nav>
-                <ul>
-                    <li>Home</li>
+                <ul> 
+                    <li>Home</li> 
                     <li>Populaire films</li>
                     <li>Genres</li>
                     <li>Trending</li>
