905634ab8c8efdec9130d03445717d91999eec66
Andre <andre.babirian2004@gmail.com>
2026-04-30 23:18:53 +0200

    13 weer begonnen met de movie pagina ik ga nu de eerste pagina aanpassen

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -6,18 +6,50 @@ function Main() {
   return (
     <div className="layout">
       <Sidebar />
-      <div className="vertical-divider" /> 
- 
+      <div className="vertical-divider" />
+
       <div className="main-page">
         <Topbar />
+        <div className="rooster-page">
+          <div className="gridContainer">
+            <div className="titleBlock">
+              <h1>???</h1>
+            </div>
 
-        <div className="content">
-          <h1>Dit is het begin</h1>
-          <p>Welkom op je mijn school dashboard</p>
+            <div className="cardsContainer">
+              <div className="card1">
+                <h1>1</h1>
+              </div>
+
+              <div className="card2">
+                <h1>2</h1>
+              </div>
+
+              <div className="card3">
+                <h1>3</h1>
+              </div>
+
+              <div className="card4">
+                <h1>4</h1>
+              </div>
+
+              <div className="card5">
+                <h1>5</h1>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
+
+
     </div>
   )
 }
 
 export default Main;
+
+
+
+
+
+
