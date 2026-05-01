b6877a775ef2852a9713e255a9b5ddc393c531df
Andre <andre.babirian2004@gmail.com>
2026-05-01 23:47:18 +0200

    24 browse pagina begonnen

--- src/pages/browse.tsx
+++ src/pages/browse.tsx
@@ -1,10 +1,30 @@
-import '../styling/browse.css' 
+import '../styling/browse.css'
 
 function Browse() {
     return (
-        <div className="Browse">
-            <h1>dit is de browser pagina </h1>
-         </div>
+        <>
+            <div className="Browse">
+                <h1>dit is de browser pagina</h1>
+            </div>
+
+            <table>
+                <tr>
+                    <th>Search</th>
+                    <th>Genres</th>
+                    <th>Format</th>
+                </tr>
+                <tr>
+                    <td> </td>
+                    <td> </td>
+                    <td></td>
+                </tr>
+                <tr>
+                    <td>  </td>
+                    <td> </td>
+                    <td></td>
+                </tr>
+            </table>
+        </>
     );
 }
 
