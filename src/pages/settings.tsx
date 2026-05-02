0c60eb7e8b651e576df6151d7c4975fe9c778301
Andre <andre.babirian2004@gmail.com>
2026-05-02 18:52:00 +0200

    40 heb de sidebar en topbar toegevoegd

--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -1,8 +1,22 @@
 import '../styling/settings.css';
+import Sidebar from "./sidebar";
+import Topbar from "./topbar";
 
 function Settings() {
     return (
-        <h1>Hallo</h1>
+        <div className="layout">
+            <Sidebar />
+            <div className="vertical-divider" />
+
+            <div className="main-page">
+                <Topbar />
+
+                <div className="rooster-page">
+                    <h1>W</h1>
+                    <p>hier komen dan alle movies die je hebt bekeken in een rij te staan </p>
+                </div>
+            </div>
+        </div>
     );
 }
 
