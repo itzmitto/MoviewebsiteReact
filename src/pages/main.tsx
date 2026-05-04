6d50d7d7d946e4d0003d322f741442c4f177a293
Andre <andre.babirian2004@gmail.com>
2026-05-04 22:14:31 +0200

    50

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -11,54 +11,31 @@ function Main() {
       <div className="main-page">
         <Topbar />
         <div className="rooster-page">
-          <div className="gridContainer">
+          
+<div className="gridContainer">
+  <div className="card1">
+    <h1>Hoofdfilm</h1>
+  </div>
 
-            <div className="cardsContainer">
-              <div className="card1">
-                <h1>dit is de main movie aan het begin van de website nog met MongoDB later connecten en skeleton toevoegen doe ik aan het einde pas wel</h1>
-              </div>
+  <h2 style={{ color: 'white', marginBottom: '10px' }}>Trending Now</h2>
 
-              <div className="card2">
-                <h1>2</h1>
-              </div>
+  <div className="cardsContainer">
+    <div className="card2"><h1>eerste film</h1></div>
+    <div className="card3"><h1>2de film</h1></div>
+    <div className="card4"><h1>en de andren</h1></div>
+    <div className="card5"><h1>en de andren</h1></div>
+    <div className="card6"><h1>en de andren</h1></div>
+    <div className="card7"><h1>en de andren</h1></div>
+    <div className="card8"><h1>en de andren</h1></div>
+  </div>
+</div>
 
-              <div className="card3">
-                <h1>3</h1>
-              </div>
-
-              <div className="card4">
-                <h1>4</h1>
-              </div>
-
-              <div className="card5">
-                <h1>5</h1>
-              </div>
-
-              <div className="card6">
-                <h1>6</h1>
-              </div>
-
-              <div className="card7">
-                <h1>7</h1>
-              </div>
-
-              <div className="card8">
-                <h1>8</h1>
-              </div>
-
-              <div className="card9">
-                <h1>9</h1>
-              </div>
-
-              <div className="card10">
-                <h1>10</h1>
-              </div>
             </div>
           </div>
         </div>
-      </div>
-    </div>
   );
 }
 
 export default Main;
+
+
