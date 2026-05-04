6ae06b0901fbb3990282419106f0819195f6faf0
Andre <andre.babirian2004@gmail.com>
2026-05-04 22:18:52 +0200

    51

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,38 +1,38 @@
-import '../styling/main.css';   
+import '../styling/main.css';
 import Sidebar from "./sidebar";
 import Topbar from "./topbar";
 
 function Main() {
   return (
-    <div className="layout"> 
+    <div className="layout">
       <Sidebar />
       <div className="vertical-divider" />
 
       <div className="main-page">
         <Topbar />
         <div className="rooster-page">
-          
-<div className="gridContainer">
-  <div className="card1">
-    <h1>Hoofdfilm</h1>
-  </div>
 
-  <h2 style={{ color: 'white', marginBottom: '10px' }}>Trending Now</h2>
+          <div className="gridContainer">
+            <div className="card1">
+              <h1>Hoofdfilm</h1>
+            </div>
 
-  <div className="cardsContainer">
-    <div className="card2"><h1>eerste film</h1></div>
-    <div className="card3"><h1>2de film</h1></div>
-    <div className="card4"><h1>en de andren</h1></div>
-    <div className="card5"><h1>en de andren</h1></div>
-    <div className="card6"><h1>en de andren</h1></div>
-    <div className="card7"><h1>en de andren</h1></div>
-    <div className="card8"><h1>en de andren</h1></div>
-  </div>
-</div>
+            <h2 style={{ color: 'white', marginBottom: '10px' }}>Trending Now</h2>
 
+            <div className="cardsContainer">
+              <div className="card2"><h1>eerste film</h1></div>
+              <div className="card3"><h1>2de film</h1></div>
+              <div className="card4"><h1>en de andren</h1></div>
+              <div className="card5"><h1>en de andren</h1></div>
+              <div className="card6"><h1>en de andren</h1></div>
+              <div className="card7"><h1>en de andren</h1></div>
+              <div className="card8"><h1>en de andren</h1></div>
             </div>
           </div>
+
         </div>
+      </div>
+    </div>
   );
 }
 
